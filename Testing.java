93d59f44629b4197ea835f02d89248acfb5420e9
Deepss <118981194+Deepssjw@users.noreply.github.com>
2023-06-26 17:36:33 +0530

    Oxford Dictionary.java

--- Testing.java
+++ Testing.java
@@ -1,7 +1,5 @@
 package org.test;
 
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,13 +8,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Testing {
 
-public static void main(String[] args) throws AWTException, IOException {
+public static void main(String[] args)
+	{
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\ELCOT\\eclipse-workspace\\Selenium TestSpace\\Driver\\chromedriver.exe");
 WebDriver dr=new ChromeDriver();
 
 dr.get("https://www.oxfordlearnersdictionaries.com/");
 dr.manage().window().maximize();
-
 
 dr.findElement(By.xpath("/html/body/div[1]/div[2]/div/form/div[2]/input")).sendKeys("fish");
 dr.findElement(By.xpath("/html/body/div[1]/div[2]/div/form/label/input")).click();
